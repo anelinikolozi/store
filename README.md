43ccc086503bfa474c75faba5e97d963824698f5
nikolozianeli <nikolozianeli@gmail.com>
2021-11-24 17:45:28 +0400

    adding private to models fixing typos

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ Project of nikolozi aneli for softlab
 1. have a pc not a potato
 2. docker-compose and docker must be installed
 ##start up
-1. go to the directory of the project and open terminal
-2. docker build -t nikosdockeri .
-3. docker-compose up
+1. go to the directory of the project and open terminal<br/>
+1)docker build -t nikosdockeri .<br/>
+2)docker-compose up
+2. connect with the database on port localhost:5431<br/> username :user<br/> password :password
+3. open console in the database<br/>
+1)create database store;<br/>
+2)open the console of database store<br/>
+3)copy in the sql script from the file database.sql
 
--- database.sql
+++ database.sql
@@ -1,28 +1,26 @@
-create database store;
-
-
-
-
-----------------------------------------------------------------------------------------------------
 create table categories
 (
-    id bigserial,
+    id bigserial
+        constraint categories_pk
+            primary key,
     parent_id bigint,
     category_name text not null
 );
 
+alter table categories owner to "user";
+
 create unique index categories_id_uindex
     on categories (id);
 
-alter table categories
-    add constraint categories_pk
-        primary key (id);
-
 create table makers
 (
-    id bigserial,
+    id bigserial
+        constraint makers_pk
+            primary key,
     "makerName" text not null
 );
+
+alter table makers owner to "user";
 
 create unique index makers_id_uindex
     on makers (id);
@@ -30,45 +28,11 @@ create unique index makers_id_uindex
 create unique index makers_makername_uindex
     on makers ("makerName");
 
-alter table makers
-    add constraint makers_pk
-        primary key (id);
-
-
-
-create table purchases
-(
-    id bigserial,
-    product_id bigint not null,
-    purchase_price numeric(10,3) not null,
-    purchase_date text not null
-);
-
-create unique index purchases_id_uindex
-    on purchases (id);
-
-alter table purchases
-    add constraint purchases_pk
-        primary key (id);
-
-create table sales
-(
-    id bigserial,
-    product_id bigint not null,
-    sell_price numeric(10,3) not null,
-    sell_date text not null
-);
-
-create unique index sales_id_uindex
-    on sales (id);
-
-alter table sales
-    add constraint sales_pk
-        primary key (id);
-
 create table products
 (
-    ean_code bigserial,
+    ean_code bigserial
+        constraint products_pk
+            primary key,
     product_name text not null,
     description text,
     maker_id bigint not null
@@ -78,28 +42,47 @@ create table products
         constraint products_categories_id_fk
             references categories,
     sell_price numeric(10,3) not null,
-    remaining int not null
+    remaining integer not null
 );
+
+alter table products owner to "user";
+
+create table purchases
+(
+    id bigserial
+        constraint purchases_pk
+            primary key,
+    product_id bigint not null
+        constraint purchases_products_ean_code_fk
+            references products,
+    purchase_price numeric(10,3) not null,
+    purchase_date timestamp
+);
+
+alter table purchases owner to "user";
+
+create unique index purchases_id_uindex
+    on purchases (id);
+
+create table sales
+(
+    id bigserial
+        constraint sales_pk
+            primary key,
+    product_id bigint not null
+        constraint sales_products_ean_code_fk
+            references products,
+    sell_price numeric(10,3) not null,
+    sell_date timestamp not null
+);
+
+alter table sales owner to "user";
+
+create unique index sales_id_uindex
+    on sales (id);
 
 create unique index products_ean_code_uindex
     on products (ean_code);
-
-alter table products
-    add constraint products_pk
-        primary key (ean_code);
-
-alter table purchases
-    add constraint purchases_products_ean_code_fk
-        foreign key (product_id) references products;
-
-alter table sales
-    add constraint sales_products_ean_code_fk
-        foreign key (product_id) references products;
-alter table purchases alter column purchase_date set default now();
-alter table purchases alter column purchase_date drop not null;
-
-alter table purchases alter column purchase_date drop default;
-
 
 
 
